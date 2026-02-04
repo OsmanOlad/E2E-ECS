506387af1628874795e37a8b2e02d9612866d563
Osman <OsmanOlad@MacBook-Pro.local>
2026-02-04 09:58:33 +0000

    fix: add lifecycle to target group to prevent delete conflict

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -44,6 +44,9 @@ resource "aws_lb_target_group" "app" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_lb_listener" "http" {
